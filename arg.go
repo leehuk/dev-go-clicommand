9718251da7f714f39bd59f6d96ae5ec8fd843b20
Lee H <lee@leeh.uk>
2018-01-27 14:08:01 +0000

    - add arg helper functions, including unbinding of args

--- arg.go
+++ arg.go
@@ -26,7 +26,35 @@ func (arg *Arg) BindCommand(cmd *Command) {
 	cmd.args = append(cmd.args, arg)
 }
 
+func (arg *Arg) UnbindCommand(cmd *Command) {
+	var newparents []*Command
+	var newargs []*Arg
+
+	for _, cmdv := range arg.parents {
+		if cmdv != cmd {
+			newparents = append(newparents, cmdv)
+		}
+	}
+
+	for _, argv := range cmd.args {
+		if argv != arg {
+			newargs = append(newargs, argv)
+		}
+	}
+
+	arg.parents = newparents
+	cmd.args = newargs
+}
+
+func (arg *Arg) GetRequired() bool {
+	return arg.required
+}
+
 func (arg *Arg) SetRequired() *Arg {
 	arg.required = true
 	return arg
+}
+
+func (arg *Arg) GetParents() []*Command {
+	return arg.parents
 }
