dc6e31f84b8d1bb2b2ad4127ca04247bab085c66
Lee H <lee@leeh.uk>
2018-02-10 17:21:32 +0000

    Squashed commit of the following:
    commit 6e1cc609caddac366ddd5ffb2dfb399c94db1ff1
    Author: Lee H <lee@leeh.uk>
    Date:   Sat Feb 10 17:21:08 2018 +0000
    
        - ensure we output command errors
    
    commit 83066e0c47026dfce8d2c87302ab82823889f217
    Author: Lee H <lee@leeh.uk>
    Date:   Sat Feb 10 16:25:22 2018 +0000
    
        - ensure help output on errors is sent to stderr
    
    commit 5198d42baa9fa1fc991533776e1f1ac1f4dffc63
    Author: Lee H <lee@leeh.uk>
    Date:   Sat Feb 10 16:19:26 2018 +0000
    
        - return command errors as an error type
    
    commit 34ae14411621d628e2a864640e580fb217cf5176
    Author: Lee H <lee@leeh.uk>
    Date:   Sat Feb 10 16:15:15 2018 +0000
    
        - output help for all errors
    
    commit 96193a475cf221ac4eeaf5f30b0e59b0b2c31553
    Author: Lee H <lee@leeh.uk>
    Date:   Tue Feb 6 20:38:32 2018 +0000
    
        - rework error handling into types and ensure we dont output from Parse()

---
+++ error.go
@@ -0,0 +1,80 @@
+package clicommand
+
+import (
+	"fmt"
+)
+
+// ErrCallback Error type for when a callback has failed.
+type ErrCallback struct {
+	data string
+}
+
+// ErrCallbackPre Error type for when a pre-validation callback has failed.
+type ErrCallbackPre struct {
+	data string
+}
+
+// ErrCommandError Error type for when a command has returned an error.
+type ErrCommandError struct {
+	data string
+}
+
+// ErrCommandInvalid Error type for when the command line uses a subcommand
+// that does not exist.
+type ErrCommandInvalid struct {
+	data string
+}
+
+// ErrCommandMissing Error type for when the command line has ended with a
+// parent command with no handler, meaning one of its children needed to be
+// chosen instead.
+type ErrCommandMissing struct{}
+
+// ErrOptionMissing Error type for when a required option is missing.
+type ErrOptionMissing struct {
+	data string
+}
+
+// ErrOptionMissingParam Error type for when the command line contains an
+// option that requires a parameter, but one is not specified
+type ErrOptionMissingParam struct {
+	data string
+}
+
+// ErrOptionUnknown Error type for when the command line contains an option,
+// that is not defined in the command tree.
+type ErrOptionUnknown struct {
+	data string
+}
+
+func (e *ErrCallback) Error() string {
+	return fmt.Sprintf("Callback error: %s", e.data)
+}
+
+func (e *ErrCallbackPre) Error() string {
+	return fmt.Sprintf("CallbackPre error: %s", e.data)
+}
+
+func (e *ErrCommandError) Error() string {
+	return fmt.Sprintf("Error: %s", e.data)
+}
+
+func (e *ErrCommandInvalid) Error() string {
+	return fmt.Sprintf("Invalid subcommand: %s", e.data)
+}
+
+func (e *ErrCommandMissing) Error() string {
+	return fmt.Sprintf("No subcommand specified")
+}
+
+func (e *ErrOptionMissing) Error() string {
+	return fmt.Sprintf("Required option missing: %s", e.data)
+}
+
+func (e *ErrOptionMissingParam) Error() string {
+	return fmt.Sprintf("Missing parameter to option: %s", e.data)
+}
+
+func (e *ErrOptionUnknown) Error() string {
+	return fmt.Sprintf("Unknown option: %s", e.data)
+}
